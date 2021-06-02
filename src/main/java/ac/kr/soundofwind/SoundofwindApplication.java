d73d7ead161682f73cbc84f9b4ac9469118e39df
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-06-02 22:32:28 +0900

    Initial commit

---
+++ src/main/java/ac/kr/soundofwind/SoundofwindApplication.java
@@ -0,0 +1,13 @@
+package ac.kr.soundofwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundofwindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundofwindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/kr/soundofwind/SoundofwindApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.kr.soundofwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundofwindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
