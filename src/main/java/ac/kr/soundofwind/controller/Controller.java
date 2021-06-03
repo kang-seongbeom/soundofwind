715dd4c044c3a4a6cd07b11e5896bb9ba695737c
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-06-03 23:56:25 +0900

    서버 구조

--- src/main/java/ac/kr/soundofwind/controller/Controller.java
+++ src/main/java/ac/kr/soundofwind/controller/Controller.java
@@ -1,4 +1,12 @@
 package ac.kr.soundofwind.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
 public class Controller {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
