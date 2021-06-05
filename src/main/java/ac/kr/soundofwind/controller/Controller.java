86ff66856654bfc5b4bb64f3df26f18d711b5627
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-06-05 21:21:54 +0900

    페이지 화면 안나오는 오류 해결
    프로젝트 내장 톰켓에 jsp를 서블릿로 처리를 해주는(수업때 배운것 처럼 jsp 또한 서블릿이다.) 기능이 없어서 jasper 의존성 추가

--- src/main/java/ac/kr/soundofwind/controller/Controller.java
+++ src/main/java/ac/kr/soundofwind/controller/Controller.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String index(){
-        return "index";
+        return "test";
     }
 }
